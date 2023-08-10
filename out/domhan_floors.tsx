26cc47d1e5a871d01e9ae17b626f697d57b1662a
Henry Price <phanxgames@gmail.com>
2023-08-10 11:35:26 -0400

    tiled ingration p2 (files)

---
+++ out/domhan_floors.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="Domhan Floors" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="804" columns="12">
+ <image source="domhan_floors.png" trans="ff00ff" width="216" height="1206"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="37" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="38" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="39" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="40" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="41" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="49" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="50" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="51" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="52" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="53" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="61" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="62" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="63" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
---
+++ out/system.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="system" tilewidth="16" tileheight="16" tilecount="216" columns="12">
+ <image source="system.png" width="192" height="288"/>
+</tileset>
